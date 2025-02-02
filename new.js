bd8b5f634610475588a7dadbe84d530a954673a6
Marwa <marwaqaisiya@gmail.com>
2025-02-02 10:39:36 +0300

    Second Hell from Marwa

--- new.js
+++ new.js
@@ -2,11 +2,6 @@ let a=2;
 let c=3;
 console.log(a+c);
 
-
-
-
-
-
 let cash = 1000 ;
 let cash2 = 500 ; 
 console.log(cash/cash2); 
@@ -22,3 +17,6 @@ for(let i=0; i<10; i++){
     console.log(i);
 }
 
+///////////////////
+
+console.log("Hello world");
